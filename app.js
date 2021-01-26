e02615d1ae86ca4d974c6a556585d5bbaca699f6
Tanja <tanja120a@gmail.com>
2021-01-27 00:28:44 +0100

    display products

--- app.js
+++ app.js
@@ -33,7 +33,28 @@ class Products {
 }
 //display products
 class UI{
-
+    displayProducts(products){
+        console.log(products);
+        let result = "";
+        products.forEach(product => {
+           result +=`
+            <!--single product-->
+            <article class="product">             
+                <div class="img-container">
+                <img src=${product.image} alt="product" class="product-img">
+                    <button class="bag-btn" data-id=${product.id}>
+                        <i class="fas fa-shopping-cart"></i>
+                        add to bag
+                    </button>                 
+                </div>
+                <h3>${product.title}</h3>
+                <h4>$${product.price}</h4>             
+            </article>
+         <!--end of single product-->
+            `
+        })
+        productsDOM.innerHTML = result;
+    }
 }
 //local storage
 class Storage{
@@ -45,7 +66,7 @@ document.addEventListener("DOMContentLoaded",() => {
     const products = new Products();
 
     //getting all Products
-    products.getProducts().then((products) => console.log(products))
+    products.getProducts().then((products) => ui.displayProducts(products))
 })
 
 
